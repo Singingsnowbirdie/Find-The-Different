1f73b93e8b8e31c99c57a70c9fd8210d47bd6a62
Elena Slavina <55747006+Singingsnowbirdie@users.noreply.github.com>
2022-03-29 09:04:37 +0300

    Project loaded

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //репозиторий
+    Repository repository;
+
+    private void Awake()
+    {
+        //создаем экземпляр репозитория
+        repository = new Repository();
+    }
+}
---
+++ Assets/Scripts/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+//репозиторий
+
+public class Repository
+{
+    //количество коллекций
+    int collectionsAmount = 2;
+
+    //все коллекции
+    Collection[] collections;
+
+    public Repository()
+    {
+        for (int i = 0; i < collectionsAmount; i++)
+        {
+            //путь к папке с коллекцией
+            string path = "Sprites/Collections/" + i.ToString();
+        }
+
+
+
+    }
+}
